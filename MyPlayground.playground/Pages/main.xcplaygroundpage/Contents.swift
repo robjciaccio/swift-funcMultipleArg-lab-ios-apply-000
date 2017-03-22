a9d6f815b5ef787f1c7d97c9e1a3ecfc4d7e86b4
robjciaccio <robjciaccio@gmail.com>
2017-03-22 13:52:27 -0400

    Lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -7,13 +7,17 @@
  
  * [Functions](https://github.com/learn-co-curriculum/swift-functions-readme)
  * [Functions with multiple arguments](https://github.com/learn-co-curriculum/swift-funcMultipleArg-readme)
-
  */
 
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
+func inTheWorld(thisContinent continent: String, contains thisMany: Int)-> String{
+    return "The chunk of land known as \(continent) contains \(thisMany) countries"
+}
+let howMany = inTheWorld(thisContinent: "The United States", contains: 23)
+print (howMany)
 
 
 
@@ -25,7 +29,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny", greeting: "Hello")
 
 
 
@@ -33,8 +37,8 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
-    print("There are \(days) in \(month)")
+func daysInMonth(month: String, days: Int) {
+    print("There are \(days) days in \(month)")
 }
 
 daysInMonth(month: "November", days: 30)
@@ -46,8 +50,11 @@ daysInMonth(month: "November", days: 30)
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
 // write your code here
-
-
+func questionFour(numberOf bottles: Int, of beverage: String, kept whereOnThe: String)-> String{
+    return "\(bottles) bottles of \(beverage) on the \(whereOnThe) wall"
+}
+let answerFour = questionFour(numberOf: 127, of: "Diet Coke", kept: "middle left")
+print (answerFour)
 
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-funcMultipleArg-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) to see the solution.
